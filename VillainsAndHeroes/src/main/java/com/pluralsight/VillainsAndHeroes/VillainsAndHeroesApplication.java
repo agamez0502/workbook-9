0a0f248050dba862174cf36a168961c21c292122
Alondra Gamez <agamez@appdev.yearup.org>
2025-06-18 10:56:06 -0500

    Completed Exercise 1 - Villains and Heroes

---
+++ VillainsAndHeroes/src/main/java/com/pluralsight/VillainsAndHeroes/VillainsAndHeroesApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.VillainsAndHeroes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VillainsAndHeroesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VillainsAndHeroesApplication.class, args);
+	}
+
+}
---
+++ VillainsAndHeroes/src/test/java/com/pluralsight/VillainsAndHeroes/VillainsAndHeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluralsight.VillainsAndHeroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VillainsAndHeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
